786f210f7c9c510149c8cc02f2bafa3429310935
strandbergryan <strandbergryan@gmail.com>
2023-10-19 19:59:00 -0600

    I have pieces that move according to some game logic

--- java/script.js
+++ java/script.js
@@ -1,26 +1,3 @@
-// alert ("java is linked")
-// class piece {
-//     constructor(color) {
-//         this.color = color;
-//         this.King = false;
-//     }
-//     makeKing() {
-//         this.King = true;
-//     }
-// }
-
-// const squares = document.querySelectorAll('.square, .square1');
-
-// let currentPlayer = "red"
-// let selectedPiece = null
-// let opponentPlayer = 'blue'
-
-// function switchPlayer() {
-//     currentPlayer = (currentPlayer === 'red') ? 'blue' : 'red';
-//     opponentPlayer = (currentPlayer === 'red') ? 'blue' : 'red';
-//     console.log(`Current Player ${currentPlayer}`);
-// }
-
 document.addEventListener('DOMContentLoaded', function () { 
     let currentPlayer = "red";
 
@@ -29,146 +6,108 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     const pieces = document.querySelectorAll(".piece");
+    const squares = document.querySelectorAll(".square1");
+    let selectedPiece = null;
+
     pieces.forEach(piece => {
         piece.addEventListener("click", function (event) {
+            if (currentPlayerPiece(piece)) {
             event.stopPropagation();
-            pieces.forEach(p => p.classList.remove("selected"));
+
+            if(selectedPiece) {
+                selectedPiece.classList.remove("selected");
+            }
+
             piece.classList.add("selected");
+            selectedPiece = piece
+            } else if (selectedPiece) {
+                const selectedSquare = document.querySelector(".square1.selected");
+                if (isValidMove(selectedPiece, selectedSquare)) {
+                    selectedSquare.appendChild(selectedPiece);
+                    selectedPiece.classList.remove("selected");
+
+                    currentPlayer = currentPlayer === "red" ? "blue" : "red";
+                    selectedPiece = null;
+                }
+            }
         });
     });
-    
-    const squares = document.querySelectorAll(".square, .square1");
+
     squares.forEach(square => {
         square.addEventListener("click", function () {
-            console.log("square is clicked")
-            const selectedPiece = document.querySelector(".selected");
-
-            if (selectedPiece) {
+            if (selectedPiece && isValidMove(selectedPiece, square)) {
                 square.appendChild(selectedPiece);
                 selectedPiece.classList.remove("selected");
                 
-                currentPlayer = currentPlayer === "red" ? "blue" : "Red";
+                currentPlayer = currentPlayer === "red" ? "blue" : "red";
+                selectedPiece = null;
              }
         });
     });
 });
 
+function isValidMove(piece, square) {
+    const pieceRow = +piece.parentElement.dataset.row;
+    const pieceCol = +piece.parentElement.dataset.col;
+    const targetRow = +square.dataset.row;
+    const targetCol = +square.dataset.col;
 
-//         } handleSquareClick(square));
-//     });
+    const rowDiff = Math.abs(targetRow - pieceRow);
+    const colDiff = Math.abs(targetCol - pieceCol);
 
-//     pieces.forEach(piece => {
-//         piece.addEventListener('click', () => handlePieceClick(piece));
-//     });
-
-//     function handlePieceClick(piece) {
-//         if (piece.classList.contains(currentPlayer)) {
-//             selectedPiece = piece;
-//         } else if (selectedPiece) {
-//             const isValidMove = isValidMove(piece);
-
-//             if (isValidMove) {
-//                 piece.appendChild(selectedPiece);
-//                 checkForCapture(piece);
-//                 checkForKinging(piece);
-//                 selectedPiece= null;
-//                 switchPlayer();
-//             }
-//         }
-//     }
-
-//     function handleSquareClick(square) {
-//         const clickedPiece = square.querySelector('.piece');
-
-//         if (clickedPiece && clickedPiece.classList.contains(currentPlayer)) {
-//             selectedPiece = clickedPiece;
-//         } else if (selectedPiece) {
-//             const isValidMove = isValidMove(square);
-
-//             if (isValidMove) {
-//             square.appendChild(selectedPiece);
-//             checkForCapture(square);
-//             checkForKinging(square);
-//             selectedPiece = null;
-//             switchPlayer();
-//             }
-//         }
-//     }
-
-//     function isValidMove(square) {
-//         const row = Math.floor(Array.from(squares).indexOf(square) / 8);
-//         const col = Array.from(squares).indexOf(square) % 8;
-//         const validMoves = getValidMoves(row, col);
+    if (rowDiff === 1 && colDiff === 1 && !square.hasChildNodes()) {
+        return true;
+    }
     
-//         // Check if the clicked square is one of the valid moves
-//         return validMoves.includes(Array.from(squares).indexOf(square));
+    return false; 
+}
+
+// function isValidKingMove(piece, square) {
+//     const pieceRow = +piece.parentElement.dataset.row;
+//     const pieceCol = +piece.parentElement.dataset.col;
+//     const targetRow = +square.dataset.row;
+//     const targetCol = +square.dataset.col;
+
+//     const rowDiff = Math.abs(targetRow - pieceRow);
+//     const colDiff = Math.abs(targetCol - pieceCol);
+
+//     if (rowDiff === 1 && colDiff === 1 && !square.hasChildNodes()) {
+//         return true;
 //     }
+//     return false; 
+// }
 
-//     function movePiece(fromSquare, toSquare) {
-//         const piece = fromSquare.querySelector('.piece');
-//         fromSquare.removeChild(piece);
-//         toSquare.appendChild(piece);
-//     }
 
-//     function getValidMoves(row, col) {
-//         const validMoves = [];
+// function removeCapturePieces(piece, desinationSquare) {
+//     const pieceRow = +piece.parentElement.dataset.row;
+//     const pieceCol = +piece.parentElement.dataset.col;
+//     const targetRow = +desinationSquare.dataset.row;
+//     const targetCol = +desinationSquare.dataset.col;
 
-//         if (currentPlayer === 'red') {
-//             if (row > 0) {
-//                 if (col > 0) validMoves.push((row - 1) * 8 + col - 1);
-//                 if (col < 7) validMoves.push((row - 1) * 8 + col + 1);
-//             }
-//         } else {
-//             if (row < 7) {
-//                 if (col > 0) validMoves.push((row + 1) * 8 + col - 1);
-//                 if (col < 7) validMoves.push((row + 1) * 8 + col + 1);
-//             }
+//     const rowDirection = targetRow - pieceRow > 0 ? 1 : -1;
+//     const colDirection = targetCol - pieceRow > 0 ? 1 : -1;
+
+//     let currentRow = pieceRow + rowDirection;
+//     let currentCol = pieceCol + colDirection;
+
+//     while (currentRow !== targetRow && currentCol !== targetCol) {
+//         const square = document.querySelector(`[data-row="${currentRow}"][data-col="${currentCol}"]`);
+//         if (square.hasChildNodes()) {
+//             square.removeChild(square.firstChild);
 //         }
-
-//         return validMoves;
+        
+//         currentRow += rowDirection;
+//         currentCol += colDirection;
 //     }
+// }
 
-//     function checkForKinging(square) {
-//         const piece = square.querySelector('.piece');
-//         const row = Math.floor(Array.from(squares).indexOf(square) / 8);
-
-//         if ((currentPlayer === 'red' && row === 0) || (currentPlayer === 'blue' && row === 7)) {
-//             piece.classList.add('king');
-//         }
+// function isKing(piece, square) {
+//     if((currentPlayer === "red" && square.dataset.row === "8") || (currentPlayer === "blue" && square.dataset.row === "1")) {
+//         return true;
 //     }
+//     return false;
+// }
 
-//     function checkForCapture(square) {
-//         const currentIndex = Array.from(squares).indexOf(square);
-//         const row = Math.floor(currentIndex / 8);
-//         const col = currentIndex % 8;
-//         const opponentPlayer = currentPlayer === 'red' ? 'blue' : 'red';
-
-//         if (currentPlayer === 'red') {
-//             if (row > 1) {
-//                 if (col > 1 && squares[(row - 1) * 8 + col - 1].querySelector('.piece')?.classList.contains(opponentPlayer)
-//                     && !squares[(row - 2) * 8 + col - 2].querySelector('.piece')) {
-//                     console.log('capture is possible!');
-//                 }
-//                 if (row > 1) {
-//                     if (col < 6 && squares[(row - 1) * 8 + col + 1].querySelector('.piece')?.classList.contains(opponentPlayer)
-//                         && !squares[(row - 2) * 8 + col + 2].querySelector('.piece')) {
-//                         console.log('capture is possible!');
-//                     }
-//                 }
-//             } else {
-//                 if (row < 6) {
-//                     if (col > 1 && squares[(row - 1) * 8 + col - 1].querySelector('.piece')?.classList.contains(opponentPlayer)
-//                         && !squares[(row - 2) * 8 + col - 2].querySelector('.piece')) {
-//                         console.log('capture is possible!');
-//                     }
-//                     if (row > 1) {
-//                         if (col < 6 && squares[(row - 1) * 8 + col + 1].querySelector('.piece')?.classList.contains(opponentPlayer)
-//                             && !squares[(row - 2) * 8 + col + 2].querySelector('.piece')) {
-//                             console.log('capture is possible!');
-//                         }
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// });
+// function kingPiece(piece) {
+//     piece.classList.add("king");
+// }
